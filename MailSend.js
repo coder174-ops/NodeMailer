f9cb4df438db5808624a927b41c5b4f41c4b344e
Neeraj <neerajuiet2023@gmail.com>
2025-06-03 16:09:44 +0530

    mail change

--- MailSend.js
+++ MailSend.js
@@ -1,21 +1,45 @@
+// const nodemailer=require('nodemailer');
+
+// const transporter=nodemailer.createTransport({
+//    secure: true,
+//    host: 'smtp.gmail.com', 
+//    port: 465, 
+//     auth: {
+//         user: 'neerajuiet2023@gmail.com',
+//         pass: 'zxnnlvdoppwbfuxc'
+//     }
+// });
+
+// function sendMail(to,sub,msg){
+//   transporter.sendMail({
+//     to: to,
+//     subject: sub,
+//     html: msg
+// });
+// console.log("Email Sent Successfully");
+// }
+// sendMail("neerajuiet2023@gmail.com","This is subject 2","I am fine")
+
 const nodemailer=require('nodemailer');
 
-const transporter=nodemailer.createTransport({
-   secure: true,
-   host: 'smtp.gmail.com', // Replace with your SMTP server
-   port: 465, // Port for secure SMTP
-    auth: {
-        user: 'neerajuiet2023@gmail.com',
-        pass: 'zxnnlvdoppwbfuxc'
+const transporter=nodemailer.createTransport(
+  {
+    secure:true,
+    host:'smtp.gmail.com',
+    port:465,
+    auth:{
+      user:'niteshgo14356@gmail.com',
+      pass:'kybgbfpairbpykgf'
     }
-});
+  }
+)
 
-function sendMail(to,sub,msg){
+function sendMail(to ,sub, msg){
   transporter.sendMail({
-    to: to,
-    subject: sub,
-    html: msg
-});
-console.log("Email Sent Successfully");
+    to:to,
+    subject : sub,
+    html:msg
+  });
+console.log('email sent')
 }
-sendMail("neerajuiet2023@gmail.com","This is subject 2","I am fine")
+sendMail("niteshgo14356@gmail.com" ,"this is subject" ,"I am fine");
